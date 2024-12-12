b8346236e4e95fcc97d33e42cbfde93a3022a41c
Leon Miszkurka <leonmiszkurka@gmail.com>
2024-12-11 18:43:43 -0800

    Adding Toolui

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Easyswing
+
+This is a LIBRAY for java and it adds...
+
+1. Easyer way to make GUI's in java
+
+Sorce code in  Tools > swing 
---
+++ Tools/swing/Toolui.java
@@ -0,0 +1,11 @@
+package Tools.swing;
+
+import javax.swing.JFrame;
+
+public class Toolui {
+    public static void ui(String name) {
+
+        JFrame JF = new JFrame(""+ name);
+        JF.show();
+    }
+}
